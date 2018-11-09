8f3e3aa952e39d92936d056e4eecf9f53d3287f4
papsiii <florian0stock@gmail.com>
2018-11-09 11:35:00 +0100

    Creation of interface UI

--- app/src/main/java/com/go4lunch/flooo/go4lunch/MapViewFragment.java
+++ app/src/main/java/com/go4lunch/flooo/go4lunch/MapViewFragment.java
@@ -5,45 +5,37 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
-import com.facebook.places.Places;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.security.Permission;
-import java.util.List;
+
 
 
 /**
@@ -68,6 +60,9 @@ public class MapViewFragment extends Fragment implements OnMapReadyCallback,Goog
     private LocationListener mlocationlistener;
     private Marker mUserMarker;
 
+    //private FusedLocationProviderClient mFusedLocationClient;
+
+    private GoogleApiClient mGoogleApiClient;
 
     public MapViewFragment()
     {
@@ -86,6 +81,13 @@ public class MapViewFragment extends Fragment implements OnMapReadyCallback,Goog
         super.onCreate(savedInstanceState);
 
         mlocationmanager = (LocationManager) this.getActivity().getSystemService(Context.LOCATION_SERVICE);
+
+        // Construct a GeoDataClient.
+        //mPlaceDetectionClient = Places.getPlaceDetectionClient(this.getActivity());
+        // Construct a FusedLocationProviderClient.
+        //mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+
 
         mlocationlistener = new LocationListener()
         {
@@ -121,14 +123,8 @@ public class MapViewFragment extends Fragment implements OnMapReadyCallback,Goog
 
 
 
-        if (ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+        if (ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
 
             ActivityCompat.requestPermissions(this.getActivity(),new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
 
@@ -174,10 +170,6 @@ public class MapViewFragment extends Fragment implements OnMapReadyCallback,Goog
         mMapView = rootView.findViewById(R.id.mapView);
         mMapView.onCreate(savedInstanceState);
         mMapView.onResume();
-
-
-
-
         mMapView.getMapAsync(this);
 
         return rootView;
@@ -215,21 +207,6 @@ public class MapViewFragment extends Fragment implements OnMapReadyCallback,Goog
     }
 
 
-
-
-        //googleMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
-
-        // For dropping a marker at a point on the Map
-        //LatLng sydney = googleMap.getMyLocation().;
-        //googleMap.addMarker(new MarkerOptions().position().title("Marker Title").snippet("Marker Description"));
-        // For zooming automatically to the location of the marker
-        //CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
-        //googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-
-
-
-
-
     private void drawMarkerUser(Location location)
     {
         if (googleMap != null)
@@ -242,7 +219,8 @@ public class MapViewFragment extends Fragment implements OnMapReadyCallback,Goog
                     .position(gps)
                     .icon(icon)
                     .title("Current Position"));
-            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(gps, 20));
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(gps, 13));
+
         }
     }
 
@@ -251,12 +229,16 @@ public class MapViewFragment extends Fragment implements OnMapReadyCallback,Goog
     {
         if(marker.equals(mUserMarker))
         {
-            Intent intent = new Intent(this.getActivity(), RestaurantProfile.class);
+            Intent intent = new Intent(this.getActivity(), RestaurantProfileActivity.class);
             intent.putExtra("LoadLayout","Notifications");
             startActivity(intent);
+            return true;
         }
 
+        return false;
     }
+
+
 
 
     public interface OnFragmentInteractionListener
--- app/src/main/java/com/go4lunch/flooo/go4lunch/RestaurantProfile.java
+++
@@ -1,8 +0,0 @@
-package com.go4lunch.flooo.go4lunch;
-
-import android.support.v7.app.AppCompatActivity;
-
-public class RestaurantProfile extends AppCompatActivity
-{
-
-}
